2b129dcb97437111872877b9ae630a1cd85e3a8d
nelfen <nelfen.dev@gmail.com>
2025-09-12 17:16:06 +0900

    폴더 이동

--- Momentum Project/Chaper01/js/Function(sayHello).js
+++
@@ -1,7 +0,0 @@
-function sayHello(nameOfperson, age) {
-    console.log("Hello my name is " + nameOfperson + " and I'm " + age);
-}
-
-sayHello("Sian", 23);
-sayHello("Lyn", 21);
-sayHello("Jack", 32);
--- Momentum Project/Chaper01/js/if(isNaN).js
+++
@@ -1,7 +0,0 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age)) {
-    console.log("please write a number");
-} else {
-    console.log(age);
-}
